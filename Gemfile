62742539a463b83e7fb59fb9bac550cce9664647
Manuel Ortega <maoc84@gmail.com>
2025-07-08 13:31:51 +0200

    Adds i18n support + english and spanish translations

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source 'https://rubygems.org'
 
 gem 'sinatra', '~> 3.0'
 gem 'sinatra-contrib', '~> 3.0'
+
+gem 'i18n'
--- app.rb
+++ app.rb
@@ -1,6 +1,25 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
+require 'i18n'
+require 'i18n/backend/fallbacks'
+
+helpers do
+  def get_default_locale
+    @env["HTTP_ACCEPT_LANGUAGE"][0,2] || I18n.default_locale
+  end
+end
+
+I18n.available_locales = [:en, :es]
+I18n.default_locale = :es
+I18n::Backend::Simple.include(I18n::Backend::Fallbacks)
+I18n.load_path = Dir[File.join(settings.root, 'locales', '*.yml')]
+I18n.backend.load_translations
+
+before do
+  I18n.locale = params[:locale] || get_default_locale
+end
 
 get '/' do
+  p @env["HTTP_ACCEPT_LANGUAGE"][0,2]
   erb :home
 end
