97f7597d8c7785c79467e7db6f26db9a86a13ac2
Manuel Ortega <maoc84@gmail.com>
2025-07-29 13:10:17 +0200

    Adds estudio ia service

--- Gemfile
+++ Gemfile
@@ -4,3 +4,6 @@ gem 'sinatra', '~> 3.0'
 gem 'sinatra-contrib', '~> 3.0'
 
 gem 'i18n'
+# Added to enable email sending from contact form
+gem 'pony'
+gem 'dotenv'
--- app.rb
+++ app.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 require 'i18n'
 require 'i18n/backend/fallbacks'
+require 'dotenv/load'
+require 'pony'
 
 helpers do
   def get_default_locale
@@ -15,11 +17,52 @@ I18n::Backend::Simple.include(I18n::Backend::Fallbacks)
 I18n.load_path = Dir[File.join(settings.root, 'locales', '*.yml')]
 I18n.backend.load_translations
 
+# Configure Pony SMTP settings if environment variables are present
+if ENV['SMTP_ADDRESS']
+  Pony.options = {
+    via: :smtp,
+    via_options: {
+      address:              ENV['SMTP_ADDRESS'],
+      port:                 ENV['SMTP_PORT'] || '587',
+      enable_starttls_auto: true,
+      user_name:            ENV['SMTP_USER'],
+      password:             ENV['SMTP_PASSWORD'],
+      authentication:       :plain,
+      domain:               ENV['SMTP_DOMAIN'] || 'localhost'
+    }
+  }
+end
+
 before do
   I18n.locale = params[:locale] || get_default_locale
 end
 
 get '/' do
-  p @env["HTTP_ACCEPT_LANGUAGE"][0,2]
   erb :home
+end
+
+get '/estudio-ia' do
+  erb :estudio_ia
+end
+
+post '/estudio-ia/contact' do
+  nombre = params[:nombre]
+  email  = params[:email]
+  telefono = params[:telefono]
+  comentarios = params[:comentarios]
+
+  Pony.mail(
+    to: 'info@gazpacho.dev',
+    from: email || 'web@gazpacho.dev',
+    subject: "Contacto Cooperativa - #{nombre}",
+    body: <<~BODY
+      Nombre de la cooperativa: #{nombre}
+      Email: #{email}
+      Teléfono: #{telefono}
+      Comentarios:
+      #{comentarios}
+    BODY
+  )
+
+  redirect to('/estudio-ia?enviado=true#contacto')
 end
